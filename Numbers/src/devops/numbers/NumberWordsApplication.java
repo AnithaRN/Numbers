6ecb24edd583a649e29f26cd7c89d6db1af630e9
AnithaRN <44571926+AnithaRN@users.noreply.github.com>
2018-10-29 11:09:20 +0000

    Update NumberWordsApplication.java

--- Numbers/src/devops/numbers/NumberWordsApplication.java
+++ Numbers/src/devops/numbers/NumberWordsApplication.java
@@ -19,6 +19,9 @@ public final class NumberWordsApplication {
 				System.out.print( "Enter number (0 to exit): " ) ;
 				String value = reader.readLine() ;
 				int number = Integer.parseInt( value ) ;
+				if(number ==0)
+					break;
+				System.out.print("The words equivalent is: " + numberWords.toWords(number));
 			} catch ( NumberFormatException | IOException e ) {
 				System.out.println( "Invalid number" ) ;
 			}
