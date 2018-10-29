d0451f3c3549520c5b90176fe4b480f3775d5325
AnithaRN <44571926+AnithaRN@users.noreply.github.com>
2018-10-29 11:10:01 +0000

    Update NumberWordsTests.java

--- Numbers/test/devops/numbers/NumberWordsTests.java
+++ Numbers/test/devops/numbers/NumberWordsTests.java
@@ -16,5 +16,17 @@ public class NumberWordsTests {
 	public void numberOutOfRangeReturnsError() {
 		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( -1 ) ) ;
 	}
-
+	@Test
+	public void number2EqualsTwo() {
+		Assert.assertEquals( "2", "Two", numberWords.toWords(2) ) ;
+	}
+	@Test
+	public void number27EqualsTwentySeven() {
+		Assert.assertEquals( "27", "Twenty Seven", numberWords.toWords(27) ) ;
+	}
+	@Test
+	public void number845EqualsEightFortyFiveTwo() {
+		Assert.assertEquals( "845", "Eight Hundred Forty Five", numberWords.toWords(845) ) ;
+	}
+	
 }
